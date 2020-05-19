36a249876df589877041df8c390e30e76e26e063
avinash10592 <59564861+avinash10592@users.noreply.github.com>
2020-05-19 18:32:40 +0530

    Add files via upload

--- SpringBoot-Product-usingExternalservice/src/main/java/com/example/controller/SpringBootExternalServiceController.java
+++ SpringBoot-Product-usingExternalservice/src/main/java/com/example/controller/SpringBootExternalServiceController.java
@@ -3,12 +3,13 @@ package com.example.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.api.Product;
 import com.example.service.SpringBootExternalService;
 
 @RestController
@@ -17,15 +18,33 @@ public class SpringBootExternalServiceController {
 	@Autowired
 	SpringBootExternalService externalService;
 
-	@GetMapping("/products")
-	public List<Object> getProducts() {
-		return externalService.getProducts();
-
+	@RequestMapping("/products")
+	public List<Product> getAllProducts() {
+		return externalService.getAllProducts();
 	}
 
+	// fetch the product by id
+	@RequestMapping(value = "/products/{id}")
+	public Product getProductById(@PathVariable int id) {
+		return externalService.getProductById(id);
+	}
+
+	// add a product
 	@RequestMapping(method = RequestMethod.POST, value = "/products")
-	public void addProduct(@RequestBody Object object) {
-		externalService.addProduct(object);
-		
+	public void addProduct(@RequestBody Product product) {
+		externalService.addProduct(product);
 	}
+
+	// update the product
+	@RequestMapping(method = RequestMethod.PUT, value = "/products/{id}")
+	public void updateProduct(@RequestBody Product product, @PathVariable int id) {
+		externalService.updateProduct(product);
+	}
+	//deletes the product
+		@RequestMapping(method= RequestMethod.DELETE, value="/products/{id}")
+		public void deleteProduct(@PathVariable String id) {
+			
+			externalService.deleteProduct(id);
+			 
+		}
 }
--- SpringBoot-Product-usingExternalservice/src/main/java/com/example/service/SpringBootExternalService.java
+++ SpringBoot-Product-usingExternalservice/src/main/java/com/example/service/SpringBootExternalService.java
@@ -4,30 +4,57 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import com.example.api.Product;
 
 @Service
+@ConfigurationProperties(prefix = "endpoint")
 public class SpringBootExternalService {
+
+	private String url;
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
 	@Autowired
 	private RestTemplate restTemplate;
 
-	public List<Object> getProducts() {
-
-		String url = "http://localhost:8081/products";
-		Object[] objects = restTemplate.getForObject(url, Object[].class);
-
-		return Arrays.asList(objects);
+	public List<Product> getAllProducts() {
+		ResponseEntity<Product[]> response = restTemplate.getForEntity(url, Product[].class);
+		return Arrays.asList(response.getBody());
 
 	}
 
-	public void addProduct(Object object) {
-		
-		String url = "http://localhost:8081/products";
-		Object[] objects = restTemplate.getForObject(url, Object[].class);
+	// add product
+	public HttpStatus addProduct(Product product) {
+		ResponseEntity<HttpStatus> response = restTemplate.postForEntity(url, product, HttpStatus.class);
+		return response.getBody();
+	}
 
-		 Arrays.asList(objects);
-		
+	// update product
+	public void updateProduct(Product product) {
+		restTemplate.put(url, product);
+	}
+
+	// deletes product
+	public void deleteProduct(String id) {
+		restTemplate.delete(url + id);
+
+	}
+
+	// get by Id
+	public Product getProductById(int id) {
+		ResponseEntity<Product> response = restTemplate.getForEntity(url + "/" + id, Product.class);
+		return response.getBody();
 	}
 }
