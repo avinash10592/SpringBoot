78e284a15cbd099c279ae9e67888243eced078dc
avinash10592 <59564861+avinash10592@users.noreply.github.com>
2020-06-04 19:37:55 +0530

    Add files via upload

--- SpringBoot-Product-usingExternalservice/src/main/java/com/example/security/SecurityConfiguration.java
+++ SpringBoot-Product-usingExternalservice/src/main/java/com/example/security/SecurityConfiguration.java
@@ -1,45 +1,66 @@
 package com.example.security;
 
-import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Autowired
-	private DataSource datasource;
+	private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	@Autowired
+	private UserDetailsService jwtUserDetailsService;
 
-		// Setting up the JDBC configuration on the auth object
-		auth.jdbcAuthentication().dataSource(datasource)
-				.usersByUsernameQuery("select username, password, enabled " + "from users " + "where username = ?")
-				.authoritiesByUsernameQuery("select username, role " + "from user_roles " + "where username= ?");
+	@Autowired
+	private JwtRequestFilter jwtRequestFilter;
 
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		// configure AuthenticationManager so that it knows from where to load
+		// user for matching credentials
+		// Use BCryptPasswordEncoder
+		auth.userDetailsService(jwtUserDetailsService).passwordEncoder(passwordEncoder());
 	}
 
-//	@Override
-//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
-	// Set your configuration on auth object
-//		auth.inMemoryAuthentication().withUser("avinash").password("1234").roles("user").and().withUser("avi")
-//				.password("9876").roles("admin");
-//		auth.userDetailsService(userDetailsService);
-//	}
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
 
 	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/myapp/products/**").hasRole("ADMIN")
-				.antMatchers("/myapp/viewWithoutEdit/**").hasAnyRole("USER", "ADMIN").antMatchers("/").permitAll().and()
-				.formLogin();
+	protected void configure(HttpSecurity httpSecurity) throws Exception {
+		// We don't need CSRF for this example
+		httpSecurity.csrf().disable()
+				// dont authenticate this particular request
+				.authorizeRequests().antMatchers("/authenticate", "/register").permitAll().
+				// all other requests need to be authenticated
+				anyRequest().authenticated().and().
+				// make sure we use stateless session; session won't be used to
+				// store user's state.
+				exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+		// Add a filter to validate the tokens with every request
+		httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
 	}
 
 }
