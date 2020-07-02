844e7405a06def7e317f4b293f2a797cd5bdea29
avinash10592 <59564861+avinash10592@users.noreply.github.com>
2020-07-02 19:39:35 +0530

    Add files via upload

---
+++ SpringBootJMSProducer-UsingAnnotations/src/main/java/com/example/SpringBootJmsProducerUsingAnnotationsApplication.java
@@ -0,0 +1,36 @@
+package com.example;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.config.RabbitMQConfiguration;
+import com.example.model.Product;
+
+@SpringBootApplication
+public class SpringBootJmsProducerUsingAnnotationsApplication implements CommandLineRunner {
+	@Autowired
+	private RabbitTemplate rabbitTemplate;
+
+	public static void main(String[] args) throws InterruptedException
+	{
+		SpringApplication.run(SpringBootJmsProducerUsingAnnotationsApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception
+	{
+		Product product = new Product();
+		product.setProductId(101);
+		product.setName("Sony Laptop");
+		product.setQuantity(20);
+		
+		System.out.println("Sending message...");
+		rabbitTemplate.convertAndSend(RabbitMQConfiguration.topicExchangeName,
+				"message_routing_key", product);
+		System.out.println("Message sent successfully...");
+	}
+
+}
